be7c8727e3ad43831457ca089b825fcdd96c6a64
Lei Wang <wanglei.okay@gmail.com>
2022-12-05 23:00:12 +0200

    #closure: #119; #python

---
+++ closure/python3/119_Pascal-s_Triangle_II.py
@@ -0,0 +1,29 @@
+from typing import Callable
+
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+
+        def row_generator() -> Callable:
+            pre_row = []
+            row = 0
+
+            def func() -> List[int]:
+                nonlocal row, pre_row
+                vals =[]
+                for i in range(0, row + 1):
+                    if i == 0 or i == row:
+                        vals.append(1)
+                    else:
+                        vals.append(pre_row[i] + pre_row[i - 1])
+                pre_row = vals 
+                row += 1
+                return vals
+
+            return func
+
+        f = row_generator()
+        for row in range(rowIndex):
+            f()
+
+        return f()
