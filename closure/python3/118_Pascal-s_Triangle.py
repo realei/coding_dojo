2bdbd3571a93339113bc080c9a7d3aa3dffeec29
Lei Wang <wanglei.okay@gmail.com>
2023-01-04 19:56:51 +0200

    fix: #closure #118

--- closure/python3/118_Pascal-s_Triangle.py
+++ closure/python3/118_Pascal-s_Triangle.py
@@ -1,17 +1,28 @@
+from typing import Callable
+
+
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         ret = []
-        def row_generator(row: int) -> List[int]:
-            vals = []
-            for i in range(0, row + 1):
-                if i == 0 or i == row:
-                    vals.append(1)
-                else:
-                    vals.append(ret[row - 1][i] + ret[row - 1][i - 1])
-                    
-            return vals
 
+        def row_generator() -> Callable:
+            row, pre_row = 0, []
+            def func() -> List[int]:
+                nonlocal row, pre_row
+                vals = []
+                for i in range(0, row + 1):
+                    if i == 0 or i == row:
+                        vals.append(1)
+                    else:
+                        vals.append(pre_row[i] + pre_row[i - 1])
+                pre_row = vals
+                row += 1
+                return vals
+
+            return func
+
+        f = row_generator()
         for row in range(numRows):
-            ret.append(row_generator(row))
+            ret.append(f())
 
-        return ret 
+        return ret
